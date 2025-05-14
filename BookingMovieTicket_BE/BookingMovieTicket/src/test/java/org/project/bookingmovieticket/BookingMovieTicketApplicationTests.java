ea2208bcf29144dc0f3e0163a5c82c5b05517471
ChauMinh2805 <97666345+ChauMinh2805@users.noreply.github.com>
2025-05-14 08:27:35 +0700

    first commit

---
+++ BookingMovieTicket_BE/BookingMovieTicket/src/test/java/org/project/bookingmovieticket/BookingMovieTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.bookingmovieticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingMovieTicketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
