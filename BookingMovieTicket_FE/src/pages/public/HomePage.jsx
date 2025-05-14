dc7116eac824b825a236b28561e612eb3d15cbd6
ChauMinh2805 <97666345+ChauMinh2805@users.noreply.github.com>
2025-05-14 08:58:01 +0700

    Update files and remove old ones

---
+++ BookingMovieTicket_FE/src/pages/public/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NavBar from "../../components/layouts/NavBar";
+import Banner from "../../components/layouts/Banner";
+function HomePage() {
+  return (
+    <>
+      <NavBar />
+      <Banner />
+    </>
+  );
+}
+
+export default HomePage;
