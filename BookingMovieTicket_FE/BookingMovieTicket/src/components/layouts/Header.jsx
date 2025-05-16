bbbadfda677e6cc6446bb6d818362627275d1bf9
ChauMinh2805 <97666345+ChauMinh2805@users.noreply.github.com>
2025-05-16 16:29:11 +0700

    update dropdown

--- BookingMovieTicket_FE/BookingMovieTicket/src/components/layouts/Header.jsx
+++ BookingMovieTicket_FE/BookingMovieTicket/src/components/layouts/Header.jsx
@@ -15,20 +15,62 @@ function Header() {
           </div>
           <div className="h-full flex items-center justify-center w-[500px]">
             <ul className="flex space-x-[15] w-full justify-around h-full">
-              <li className="text-[24px] cursor-pointer hover:border-b-2 hover:border-black h-full items-center flex justify-center">
+              <li className="text-[24px] cursor-pointer hover:border-b-2 hover:border-black h-full w-30 items-center flex justify-center">
                 Trang Chủ
               </li>
-              <li className="text-[24px] cursor-pointer hover:border-b-2 hover:border-black h-full items-center flex justify-center">
+              <li className="text-[24px] cursor-pointer hover:border-b-2 hover:border-black h-full w-30 items-center flex justify-center">
                 Phim
               </li>
-              <li className="text-[24px] cursor-pointer hover:border-b-2 hover:border-black h-full items-center flex justify-center">
+              <li className="text-[24px] cursor-pointer hover:border-b-2 hover:border-black h-full w-30 items-center flex justify-center">
                 Lịch chiếu
               </li>
             </ul>
           </div>
-          <div className="flex items-center justify-around w-[250px] cursor-pointer">
-            <IoPersonCircle className="w-8 h-16" />
-            <p className="text-[24px]">Nguyen Van Anh</p>
+          <button
+            id="dropdownDefaultButton"
+            data-dropdown-toggle="dropdown"
+            className=""
+            type="button"
+          >
+            <div className="flex items-center justify-around w-[250px] cursor-pointer">
+              <IoPersonCircle className="w-8 h-16" />
+              <p className="text-[24px]">Nguyen Van Anh</p>
+            </div>
+          </button>
+
+          <div
+            id="dropdown"
+            className="z-40 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-60 dark:bg-gray-700"
+          >
+            <ul
+              className="py-2 text-sm text-gray-700 dark:text-gray-200"
+              aria-labelledby="dropdownDefaultButton"
+            >
+              <li>
+                <a
+                  href="#"
+                  className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                >
+                  Thông tin tài khoản
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                >
+                  Đổi mặt khẩu
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                >
+                  Đăng xuất
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
