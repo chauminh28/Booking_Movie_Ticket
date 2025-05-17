83c1b82c8717ee2e4df910e04c6f18026a84308d
kazuha1234 <baty9589@gmail.com>
2025-05-17 10:52:44 +0700

    update UI RoomMangaer

--- BookingMovieTicket_FE/BookingMovieTicket/src/App.jsx
+++ BookingMovieTicket_FE/BookingMovieTicket/src/App.jsx
@@ -3,8 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/features/Login";
 import Register from "./components/features/Register";
 import ForgetPassword from "./components/features/ForgetPassword";
-import NavbarAdmin from "./components/layouts/NavbarAdmin";
+import RoomManger from "./components/features/RoomManager/RoomManger";
 import HomePage from "./pages/public/HomePage";
+import NavbarAdmin from "./components/layouts/NavbarAdmin";
+
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/forgetPassword" element={<ForgetPassword />} />
+        <Route path="/roomManager" element={<RoomManger />} />
         <Route path="/navbar" element={<NavbarAdmin />} />
       </Routes>
     </Router>
