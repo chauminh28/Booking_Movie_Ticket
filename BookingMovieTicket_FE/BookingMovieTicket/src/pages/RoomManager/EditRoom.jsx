7107ee545410767efa2b686ca85279a0dcd53fb1
ChauMinh2805 <97666345+ChauMinh2805@users.noreply.github.com>
2025-05-23 10:05:58 +0700

    Add UI SeatManager

--- BookingMovieTicket_FE/BookingMovieTicket/src/pages/RoomManager/EditRoom.jsx
+++ BookingMovieTicket_FE/BookingMovieTicket/src/pages/RoomManager/EditRoom.jsx
@@ -3,6 +3,9 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import NavbarAdmin from "../../components/layouts/NavbarAdmin";
 import HeaderAdmin from "../../components/layouts/HeaderAdmin";
+import { IoMdAddCircle } from "react-icons/io";
+import { MdDelete, MdEdit } from "react-icons/md";
+import { LuListCollapse } from "react-icons/lu";
 
 export default function AddRoom() {
   const [row, setRow] = useState(0);
@@ -195,6 +198,266 @@ export default function AddRoom() {
                   </button>
                 </div>
               </form>
+              <div className="flex items-center justify-between mt-8 border-t-4 border-gray-400 shadow-md p-2">
+                <p className="font-bold text-xl">Ghế</p>
+                <button
+                  data-collapse-toggle="collapseActor"
+                  type="button"
+                  className="font-medium items-start cursor-pointer flex"
+                >
+                  <LuListCollapse />
+                </button>
+              </div>
+              <div id="collapseActor" className="hidden">
+                <div className="font-medium  dark:text-gray-200">
+                  <div className="flex items-center w-full">
+                    <table className="table-auto w-full text-left text-sm">
+                      <thead>
+                        <tr className="font-semibold text-[15px] text-[#A2A2A6]">
+                          <th className="px-4 py-2">Tên ghế</th>
+                          <th className="px-4 py-2">Hàng ghế</th>
+                          <th className="px-4 py-2">Loại ghế</th>
+                          <th className="px-4 py-2 flex items-end justify-end">
+                            <button
+                              data-modal-target="crud-modal"
+                              data-modal-toggle="crud-modal"
+                            >
+                              <IoMdAddCircle className="w-8 h-8 cursor-pointer text-blue-600 hover:text-blue-800" />
+                            </button>
+                            <div
+                              id="crud-modal"
+                              tabIndex="-1"
+                              aria-hidden="true"
+                              className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
+                            >
+                              <div className="relative p-4 w-full max-w-md max-h-full">
+                                <div className="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
+                                  <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200">
+                                    <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                                      Thêm ghế
+                                    </h3>
+                                    <button
+                                      type="button"
+                                      className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                      data-modal-toggle="crud-modal"
+                                    >
+                                      <svg
+                                        className="w-3 h-3"
+                                        aria-hidden="true"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 14 14"
+                                      >
+                                        <path
+                                          stroke="currentColor"
+                                          strokeLinecap="round"
+                                          strokeLinejoin="round"
+                                          strokeWidth="2"
+                                          d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                                        />
+                                      </svg>
+                                      <span className="sr-only">
+                                        Close modal
+                                      </span>
+                                    </button>
+                                  </div>
+                                  <form className="p-4 md:p-5">
+                                    <div className="grid gap-4 mb-4 grid-cols-2">
+                                      <div className="col-span-2">
+                                        <label
+                                          htmlFor="actor_name"
+                                          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                        >
+                                          Tên ghế
+                                        </label>
+                                        <div className="flex justify-around">
+                                          <div className="col-span-1">
+                                            <label
+                                              htmlFor="row"
+                                              className="block text-sm font-medium text-gray-700"
+                                            >
+                                              Tên hàng
+                                            </label>
+                                            <input
+                                              type="text"
+                                              id="row"
+                                              placeholder="Tên hàng"
+                                              className="bg-[#F9F9F9] mt-1 block w-[125px] px-4 py-3 rounded-lg border border-gray-300 focus:ring-2 focus:ring-blue-500 outline-none transition"
+                                              required
+                                            />
+                                          </div>
+                                          <div className="col-span-1">
+                                            <label
+                                              htmlFor="column"
+                                              className="block text-sm font-medium text-gray-700"
+                                            >
+                                              Số cột
+                                            </label>
+                                            <input
+                                              type="text"
+                                              id="column"
+                                              placeholder="Số cột"
+                                              className="bg-[#F9F9F9] mt-1 block w-[125px] px-4 py-3 rounded-lg border border-gray-300 focus:ring-2 focus:ring-blue-500 outline-none transition"
+                                              required
+                                            />
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                    <button
+                                      type="submit"
+                                      className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                    >
+                                      <svg
+                                        className="me-1 -ms-1 w-5 h-5"
+                                        fill="currentColor"
+                                        viewBox="0 0 20 20"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                      >
+                                        <path
+                                          fillRule="evenodd"
+                                          d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                                          clipRule="evenodd"
+                                        ></path>
+                                      </svg>
+                                      Thêm
+                                    </button>
+                                  </form>
+                                </div>
+                              </div>
+                            </div>
+                          </th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr className="border-t border-[#EEEEEE]">
+                          <td className="px-4 py-2">A1</td>
+                          <td className="px-4 py-2">A</td>
+                          <td className="px-4 py-2">Thường</td>
+
+                          <td className="px-4 py-2 flex space-x-4">
+                            <button
+                              className="text-blue-600 hover:text-blue-800 text-[20px] cursor-pointer"
+                              data-modal-target="crud-modal-edit"
+                              data-modal-toggle="crud-modal-edit"
+                            >
+                              <MdEdit />
+                            </button>
+                            <div
+                              id="crud-modal-edit"
+                              tabIndex="-1"
+                              aria-hidden="true"
+                              className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
+                            >
+                              <div className="relative p-4 w-full max-w-md max-h-full">
+                                <div className="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
+                                  <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200">
+                                    <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+                                      Sửa ghế
+                                    </h3>
+                                    <button
+                                      type="button"
+                                      className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                      data-modal-toggle="crud-modal-edit"
+                                    >
+                                      <svg
+                                        className="w-3 h-3"
+                                        aria-hidden="true"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 14 14"
+                                      >
+                                        <path
+                                          stroke="currentColor"
+                                          strokeLinecap="round"
+                                          strokeLinejoin="round"
+                                          strokeWidth="2"
+                                          d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                                        />
+                                      </svg>
+                                      <span className="sr-only">
+                                        Close modal
+                                      </span>
+                                    </button>
+                                  </div>
+                                  <form className="p-4 md:p-5">
+                                    <div className="grid gap-4 mb-4 grid-cols-2">
+                                      <div className="col-span-2">
+                                        <label
+                                          htmlFor="actor_name"
+                                          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                        >
+                                          Sửa ghế
+                                        </label>
+                                        <div className="flex justify-around">
+                                          <div className="col-span-1">
+                                            <label
+                                              htmlFor="row"
+                                              className="block text-sm font-medium text-gray-700"
+                                            >
+                                              Tên hàng
+                                            </label>
+                                            <input
+                                              type="text"
+                                              id="row"
+                                              placeholder="Tên hàng"
+                                              className="bg-[#F9F9F9] mt-1 block w-[125px] px-4 py-3 rounded-lg border border-gray-300 focus:ring-2 focus:ring-blue-500 outline-none transition"
+                                              required
+                                            />
+                                          </div>
+                                          <div className="col-span-1">
+                                            <label
+                                              htmlFor="column"
+                                              className="block text-sm font-medium text-gray-700"
+                                            >
+                                              Số cột
+                                            </label>
+                                            <input
+                                              type="text"
+                                              id="column"
+                                              placeholder="Số cột"
+                                              className="bg-[#F9F9F9] mt-1 block w-[125px] px-4 py-3 rounded-lg border border-gray-300 focus:ring-2 focus:ring-blue-500 outline-none transition"
+                                              required
+                                            />
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                    <button
+                                      type="submit"
+                                      className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                    >
+                                      <svg
+                                        className="me-1 -ms-1 w-5 h-5"
+                                        fill="currentColor"
+                                        viewBox="0 0 20 20"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                      >
+                                        <path
+                                          fillRule="evenodd"
+                                          d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                                          clipRule="evenodd"
+                                        ></path>
+                                      </svg>
+                                      Lưu
+                                    </button>
+                                  </form>
+                                </div>
+                              </div>
+                            </div>
+                            <button
+                              className="text-red-600 hover:text-red-800 text-[20px] cursor-pointer"
+                              onClick={() => alert("Xóa thành công")}
+                            >
+                              <MdDelete />
+                            </button>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
